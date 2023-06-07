161c297011723e30e8325f0fd3592267ede2998f
lewiscasewell <email>
2023-06-06 21:59:32 -0700

    make name changes to openweight

--- app/ios/OpenweightTests/OpenweightTests.m
+++ app/ios/OpenweightTests/OpenweightTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface appTests : XCTestCase
+@interface OpenweightTests : XCTestCase
 
 @end
 
-@implementation appTests
+@implementation OpenweightTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
