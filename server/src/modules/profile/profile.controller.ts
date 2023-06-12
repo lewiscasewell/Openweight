a9433ee0592488e44dab3bcc364e847dbe1cd334
lewiscasewell <email>
2023-06-12 14:06:46 +0100

    added feature for deleting a use

--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -3,6 +3,12 @@ import { db } from "../../db/database";
 import { v4 as uuidV4 } from "uuid";
 import dayjs from "dayjs";
 
+type DeleteProfileFastifyRequest = FastifyRequest<{
+  Params: {
+    id: string;
+  };
+}>;
+
 export async function fetchProfileHandler(
   request: FastifyRequest,
   reply: FastifyReply
@@ -50,4 +56,42 @@ export async function fetchProfileHandler(
       updated_at: dayjs(profile.updated_at).valueOf(),
     },
   });
+}
+
+export async function deleteProfileHandler(
+  request: DeleteProfileFastifyRequest,
+  reply: FastifyReply
+) {
+  const { id } = request.params;
+
+  const profile = await db
+    .selectFrom("profiles")
+    .selectAll()
+    .where("id", "=", id)
+    .executeTakeFirst();
+
+  if (!profile) {
+    return reply.code(404).send({ message: "Profile not found" });
+  }
+
+  try {
+    const { data, error } = await request.supabaseClient.auth.admin.deleteUser(
+      profile.supabase_user_id
+    );
+
+    if (error) {
+      console.log(error);
+      return reply.code(500).send({ message: "Server error" });
+    }
+
+    console.log(data);
+
+    await db.deleteFrom("profiles").where("id", "=", id).execute();
+    await db.deleteFrom("weights").where("profile_id", "=", id).execute();
+
+    return reply.code(200).send({ message: "Profile deleted" });
+  } catch (error) {
+    console.log(error);
+    return reply.code(500).send({ message: "Server error" });
+  }
 }
